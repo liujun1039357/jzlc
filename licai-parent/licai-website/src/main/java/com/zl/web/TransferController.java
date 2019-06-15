be0fd5844525a4fb9ca44f8e1e258b210d0db9cf
T4nka1 <1229919695@qq.com>
2019-06-15 09:51:34 +0800

    合并冲突

--- licai-parent/licai-website/src/main/java/com/zl/web/TransferController.java
+++ licai-parent/licai-website/src/main/java/com/zl/web/TransferController.java
@@ -151,41 +151,36 @@ public class TransferController {
 		return "personal/receivableRecords";
 	}
 	
-/*	*//***//*
-	@RequestMapping("recharge")
+	/***/
+	/*@RequestMapping("recharge")
 	public String recharge(BigDecimal money,String cardId, Model model) {
 
 		List<String> bankCardList = bankCardInfoService.queryCardId();
 		model.addAttribute("bankCardList",bankCardList);
 		System.out.println("bankCardList"+bankCardList);
 		if(null != money && null != cardId) {
-			long bitState = consumerInfoService.queryBitState();
-			Boolean cardyes = BitStateUtil.hasState(bitState, BitStateUtil.OPEN_BANKCARD);
-			if(cardyes) {
-				// TODO 支付密码验证
-				//银行卡余额判断
-				if(money.compareTo(new BigDecimal(5035))==1) {
-					model.addAttribute("message","银行卡余额不足，请重试。");
-					System.out.println("银行卡余额不足，请重试。");
+			//银行卡余额判断
+			if(money.compareTo(new BigDecimal(5035))==1) {
+				model.addAttribute("message","银行卡余额不足，请重试。");
+				System.out.println("银行卡余额不足，请重试。");
+			}else {
+				//获取账户余额进行充值
+				Boolean flag = consumerInfoService.recharge(money);
+				if(flag) {
+					model.addAttribute("message","支付已成功！");
+					System.out.println("支付已成功！");
 				}else {
-					//获取账户余额进行充值
-					Boolean flag = consumerInfoService.recharge(money);
-					if(flag) {
-						model.addAttribute("message","支付已成功！");
-						System.out.println("支付已成功！");
-					}else {
-						model.addAttribute("message","支付失败，请重试。");
-						System.out.println("支付失败，请重试。");
-					}
+					model.addAttribute("message","支付失败，请重试。");
+					System.out.println("支付失败，请重试。");
 				}
 			}
 		}
 		return "personal/recharge";
-	}
-	*/
-	@RequestMapping("recharge")
+	}*/
+	
+@RequestMapping("recharge")
 	@ResponseBody
-	public AjaxJson recharge1(BigDecimal money,String cardId, Model model) {
+	public AjaxJson recharge(BigDecimal money,String cardId, Model model) {
 		AjaxJson json = new AjaxJson();
 		//获取银行卡供用户选择
 		List<String> bankCardList = bankCardInfoService.queryCardId();
@@ -193,7 +188,6 @@ public class TransferController {
 		System.out.println("bankCardList"+bankCardList);
 		try {
 			if(money != null) {
-
 				if(money.compareTo(new BigDecimal(5035))==1) {
 					json.setSuccess(false);
 					json.setMsg("银行卡余额不足，请重试.");
@@ -209,12 +203,37 @@ public class TransferController {
 			json.setSuccess(false);
 			json.setMsg("系统忙!稍后重试...");
 		}
-		return json;
-				
+		return json;	
 	}
 	
 	/***/
 	@RequestMapping("cashOut")
+	public String cashOut(BigDecimal money,String cardId, Model model) {
+		
+		//获取银行卡供用户选择
+		List<String> bankCardList = bankCardInfoService.queryCardId();
+		model.addAttribute("bankCardList",bankCardList);
+		//获取账户余额进行回显
+		BigDecimal balance = consumerInfoService.queryBalance();
+		model.addAttribute("balance",balance);
+		
+		if(null != money && null != cardId) {
+			//获取账户余额进行提现
+			Boolean flag = consumerInfoService.cashOut(money);
+			if(flag) {
+				BigDecimal balance1 = consumerInfoService.queryBalance();
+				model.addAttribute("message","提现已成功！");
+				model.addAttribute("balance1",balance1);
+				System.out.println("提现已成功！");
+			}else {
+				model.addAttribute("message","提现失败，请重试。");
+				System.out.println("提现失败，请重试。");
+			}
+		}
+		return "personal/cashOut";
+	}
+	
+	/*@RequestMapping("cashOut")
 	@ResponseBody
 	public AjaxJson cashOut(BigDecimal money,String cardId, Model model) {
 		AjaxJson json = new AjaxJson();
@@ -238,9 +257,8 @@ public class TransferController {
 				json.setMsg("系统忙!稍后重试...");
 			}
 		}
-		
 		return json;
-	}
+	}*/
 
 	/***/
 	@RequestMapping("myMoney")
